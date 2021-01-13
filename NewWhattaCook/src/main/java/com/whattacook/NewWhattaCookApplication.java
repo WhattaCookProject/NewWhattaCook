fc209e35ab0f7e93a6a2c0e213c88829728133e3
Fauno Guazina <prof.guazina@gmail.com>
2021-01-13 21:30:48 +0100

    starter project

---
+++ NewWhattaCook/src/main/java/com/whattacook/NewWhattaCookApplication.java
@@ -0,0 +1,13 @@
+package com.whattacook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWhattaCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewWhattaCookApplication.class, args);
+	}
+
+}
