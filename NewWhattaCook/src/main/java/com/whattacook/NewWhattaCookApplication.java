3ce6feab8a6a6a1e1223e9a2b42dc34277535218
Fauno Guazina <prof.guazina@gmail.com>
2021-01-14 21:40:07 +0100

    basic data configuration

--- NewWhattaCook/src/main/java/com/whattacook/NewWhattaCookApplication.java
+++ NewWhattaCook/src/main/java/com/whattacook/NewWhattaCookApplication.java
@@ -2,8 +2,10 @@ package com.whattacook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class NewWhattaCookApplication {
 
 	public static void main(String[] args) {
