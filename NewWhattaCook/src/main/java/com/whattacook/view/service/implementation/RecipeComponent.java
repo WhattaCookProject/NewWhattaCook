851c5ab58ee1c44c2db4541ba14f8616c94374a2
Fauno Guazina <prof.guazina@gmail.com>
2021-01-16 22:16:02 +0100

    example of implementing component methods

--- NewWhattaCook/src/main/java/com/whattacook/view/service/implementation/RecipeComponent.java
+++ NewWhattaCook/src/main/java/com/whattacook/view/service/implementation/RecipeComponent.java
@@ -1,17 +1,24 @@
 package com.whattacook.view.service.implementation;
 
-import java.util.List;
+import static com.whattacook.util.exceptions.RecipeExceptions.throwsUp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.whattacook.model.recipe.Recipe;
+import com.whattacook.model.recipe.RecipeManager;
+
+import reactor.core.publisher.Flux;
 
 @Component
 class RecipeComponent {
+	
+	@Autowired
+	private RecipeManager manager;
 
-	List<Recipe> findAllRecipe() {
-		// TODO Auto-generated method stub
-		return null;
+	Flux<Recipe> findAllRecipe() {
+		return manager.findAll()
+				.switchIfEmpty(Flux.error(throwsUp("Sorry, there's nothing to cook")));
 	}
 
 }
