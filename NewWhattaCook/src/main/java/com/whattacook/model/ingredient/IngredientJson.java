e5e7ec426f40857a78f240bdaed295b0968d0e95
MartinG <martinjavierguilhem88@gmail.com>
2021-01-20 13:57:37 +0100

    IngredientJson

--- NewWhattaCook/src/main/java/com/whattacook/model/ingredient/IngredientJson.java
+++ NewWhattaCook/src/main/java/com/whattacook/model/ingredient/IngredientJson.java
@@ -1,5 +1,35 @@
 package com.whattacook.model.ingredient;
 
 public class IngredientJson {
+	
+	private String id;
+	private String name;
+	
+	
+	public IngredientJson() {
+	}
+
+	public IngredientJson(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+		
 
 }
