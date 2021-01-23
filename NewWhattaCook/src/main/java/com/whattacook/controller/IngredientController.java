2faa48b5ebeea8803630433321e4a0c79828486a
Fauno Guazina <prof.guazina@gmail.com>
2021-01-23 21:09:56 +0100

    eliminating import of DTO from the controller

--- NewWhattaCook/src/main/java/com/whattacook/controller/IngredientController.java
+++ NewWhattaCook/src/main/java/com/whattacook/controller/IngredientController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.whattacook.model.ingredient.Ingredient;
 import com.whattacook.model.ingredient.IngredientJson;
 import com.whattacook.view.service.implementation.IngredientService;
 
