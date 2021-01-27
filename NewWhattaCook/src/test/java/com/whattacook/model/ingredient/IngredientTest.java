feb967cf9dd8b823b17a428c465ace730b5c3a01
MartinG <martinjavierguilhem88@gmail.com>
2021-01-27 02:24:26 +0100

    add IngredientJsonTest in IngredientTest

--- NewWhattaCook/src/test/java/com/whattacook/model/ingredient/IngredientTest.java
+++ NewWhattaCook/src/test/java/com/whattacook/model/ingredient/IngredientTest.java
@@ -14,21 +14,29 @@ class IngredientTest {
 	private Ingredient ingredient;
 	private Ingredient ingredient2;
 	
+	private IngredientJson ingredientJson;
+	private IngredientJson ingredientJson2;
+
 
 	@BeforeEach
 	void setUp() throws Exception {
 		id = "ascdvfbgnhjm2345678";
 		name = "potato";
+		
 		ingredient = new Ingredient(id, name);
 		ingredient2 = new Ingredient();
 		ingredient2.setId(id);
 		ingredient2.setName(name);
 		
+		ingredientJson = new IngredientJson(id, name);
+		ingredientJson2 = new IngredientJson();
+		ingredientJson2.setId(id);
+		ingredientJson2.setName(name);
 	}
 
 	@Test
-	@DisplayName("Getters and Setters TESTING")
-	void gettersAndSettersTest() {
+	@DisplayName("Getters&Setters Ingredient")
+	void gettersSettersIngredientTest() {
 		Ingredient toCompare = new Ingredient("ascdvfbgnhjm2345678", "potato");
 		
 		assertAll(
@@ -37,7 +45,19 @@ class IngredientTest {
 				() -> assertEquals(ingredient, toCompare, "equals3"),
 				() -> assertEquals(ingredient2, toCompare, "equals4")
 				);
+	}
+	
+	@Test
+	@DisplayName("Getters and Setters IngredientJson")
+	void getteresSettersIngredientJsonTest() {
+		IngredientJson toCompare = new IngredientJson("ascdvfbgnhjm2345678", "potato");
 		
+		assertAll(
+				() -> assertEquals(id, toCompare.getId(), "equals1"),
+				() -> assertEquals(name, toCompare.getName(), "equals2"),
+				() -> assertEquals(ingredientJson, toCompare, "equals3"),
+				() -> assertEquals(ingredientJson2, toCompare, "equals3")
+				);
 	}
 
 }
