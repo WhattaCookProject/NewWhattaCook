c11e1621595619d5621f7b7367de3fc6351b069b
Fauno Guazina <prof.guazina@gmail.com>
2021-01-27 21:54:52 +0100

    setup variables

--- NewWhattaCook/src/test/java/com/whattacook/model/ingredient/IngredientManagerTest.java
+++ NewWhattaCook/src/test/java/com/whattacook/model/ingredient/IngredientManagerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.annotation.Rollback;
 
@@ -15,9 +16,21 @@ import org.springframework.test.annotation.Rollback;
 @TestMethodOrder(OrderAnnotation.class)
 class IngredientManagerTest {
 	
+	@Autowired
+	private IngredientManager manager;
+	
+	
+	private final String NOT_EXIST = "string impossible in DB";
+	private final String NAME = "AbraCadaBra"; 
+	private final Ingredient ingredient = new Ingredient(id, NAME);
+	private static String id;
+	private Boolean exist;
+	private Ingredient toCompare;
 
 	@BeforeEach
 	public void setUpBeforeEach() throws Exception {
+		toCompare = null;
+		exist = null;
 	}
 
 }
