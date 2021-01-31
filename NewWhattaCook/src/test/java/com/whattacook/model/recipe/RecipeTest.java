2941bbff20671c460ca18fab516b7c9b69031502
Fauno Guazina <prof.guazina@gmail.com>
2021-01-31 20:19:02 +0100

    improvement of DTO tests and implementation of JSON tests

--- NewWhattaCook/src/test/java/com/whattacook/model/recipe/RecipeTest.java
+++ NewWhattaCook/src/test/java/com/whattacook/model/recipe/RecipeTest.java
@@ -1,67 +1,94 @@
 package com.whattacook.model.recipe;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.whattacook.model.KitchenFactory.emptyRecipe;
+import static com.whattacook.model.KitchenFactory.emptyRecipeJson;
+import static com.whattacook.model.KitchenFactory.fiveIngredientsJsonOnSortedSet;
+import static com.whattacook.model.KitchenFactory.fiveIngredientsOnSortedSet;
+import static com.whattacook.model.KitchenFactory.fullyRecipe;
+import static com.whattacook.model.KitchenFactory.fullyRecipeJson;
+import static com.whattacook.model.KitchenFactory.getId;
+import static com.whattacook.model.KitchenFactory.getInstructions;
+import static com.whattacook.model.KitchenFactory.getTitle;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.SortedSet;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.whattacook.model.ingredient.Ingredient;
+import com.whattacook.model.ingredient.IngredientJson;
 
 class RecipeTest {
-	private String id;
-	private String title;
-	private String instructions;
-//	private Set<Ingredient> ingredientIds;
-	private Recipe recipe;
-	private Recipe recipe2;
-	private RecipeJson recipeJson;
-	private RecipeJson recipeJson2;
-	
-	
+
+	private final Recipe recipe = fullyRecipe();
+	private final String id = getId();
+	private final String title = getTitle();
+	private final String instructions = getInstructions();
+	private final SortedSet<Ingredient> ingredients = fiveIngredientsOnSortedSet();
+	private final SortedSet<IngredientJson> ingredientsJson = fiveIngredientsJsonOnSortedSet();
+	private final RecipeJson recipeJson = fullyRecipeJson();
+	private Recipe toCompare;
+	private RecipeJson toCompareJson;
+
 	@BeforeEach
 	void setUp() throws Exception {
-		id = "ascdvfbgnhjm2345678";
-		title = "Lasagna";
-		instructions = "This is how you do it";
-		recipe = new Recipe(id, title, instructions);
-		recipe2 = new Recipe();
-		recipe2.setId(id);
-		recipe2.setTitle(title);
-		recipe2.setInstructions(instructions);
-		recipeJson = new RecipeJson(id, title, instructions);
-		recipeJson2 = new RecipeJson();
-		recipeJson2.setId(id);
-		recipeJson2.setTitle(title);
-		recipeJson2.setInstructions(instructions);
+		toCompare = null;
+		toCompareJson = null;
 	}
 
 	@Test
 	@DisplayName("Getters and Setters Recipe")
 	void gettersSettersRecipeTest() {
-		Recipe toCompare = new Recipe("ascdvfbgnhjm2345678", "Lasagna","This is how you do it");
-		
-		assertAll(
-				() -> assertEquals(id, toCompare.getId(), "equals1"),
-				() -> assertEquals(title, toCompare.getTitle(), "equals2"),
-				() -> assertEquals(instructions, toCompare.getInstructions(), "equals3"),
-				() -> assertEquals(recipe, toCompare, "equals4"),
-				() -> assertEquals(recipe2, toCompare, "equals5")
+		toCompare = new Recipe(id, title, instructions, ingredients);
+		Recipe toCompare2 = emptyRecipe();
+		toCompare2.setId(id);
+		toCompare2.setTitle(title);
+		toCompare2.setInstructions(instructions);
+		toCompare2.setIngredients(ingredients);
+
+		assertAll(() -> assertEquals(id, toCompare.getId(), "Equals 1"),
+				() -> assertEquals(title, toCompare.getTitle(), "Equals 2"),
+				() -> assertEquals(instructions, toCompare.getInstructions(), "Equals 3"),
+				() -> assertEquals(ingredients, toCompare.getIngredients(), "Equals 4"),
+				() -> assertEquals(recipe, toCompare, "Equals 5"), 
+				() -> assertEquals(recipe, toCompare2, "Equals 6")
 				);
 	}
-	
+
 	@Test
 	@DisplayName("Getters and Setters RecipeJson")
 	void gettersSettersRecipeJsonTest() {
-		RecipeJson toCompare = new RecipeJson("ascdvfbgnhjm2345678", "Lasagna","This is how you do it");
-		
-		assertAll(
-				() -> assertEquals(id, toCompare.getId(), "equals1"),
-				() -> assertEquals(title, toCompare.getTitle(), "equals2"),
-				() -> assertEquals(instructions, toCompare.getInstructions(), "equals3"),
-				() -> assertEquals(recipeJson, toCompare, "equals4"),
-				() -> assertEquals(recipeJson2, toCompare, "equals5")
+		toCompareJson = new RecipeJson(id, title, instructions, ingredientsJson);
+		RecipeJson toCompareJson2 = emptyRecipeJson();
+		toCompareJson2.setId(id);
+		toCompareJson2.setTitle(title);
+		toCompareJson2.setInstructions(instructions);
+		toCompareJson2.setIngredients(ingredientsJson);
+
+		assertAll(() -> assertEquals(id, toCompareJson.getId(), "Equals 1"),
+				() -> assertEquals(title, toCompareJson.getTitle(), "Equals 2"),
+				() -> assertEquals(instructions, toCompareJson.getInstructions(), "Equals 3"),
+				() -> assertEquals(ingredientsJson, toCompareJson.getIngredients(), "Equals 4"),
+				() -> assertEquals(recipeJson, toCompareJson, "Equals 5"),
+				() -> assertEquals(recipeJson, toCompareJson2, "Equals 6")
 				);
+	}
+
+	@Test
+	@DisplayName("Recipe to Json")
+	void teste_RecipeToJson() {
+		toCompareJson = recipe.toJson();
+		assertEquals(recipeJson, toCompareJson, "Equals");
+	}
+
+	@Test
+	@DisplayName("Json to Recipe")
+	void teste_JsonToRecipe() {
+		toCompare = recipeJson.toRecipe();
+		assertEquals(recipe, toCompare, "Equals 5");
 	}
 
 }
