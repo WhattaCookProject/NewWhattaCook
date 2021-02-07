85a1fd916cf910d207dffff48ccbaccc3839754d
Fauno Guazina <prof.guazina@gmail.com>
2021-02-07 21:21:53 +0100

    creation of new query methods

--- NewWhattaCook/src/main/java/com/whattacook/model/recipe/RecipeManager.java
+++ NewWhattaCook/src/main/java/com/whattacook/model/recipe/RecipeManager.java
@@ -3,7 +3,14 @@ package com.whattacook.model.recipe;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @Repository
 public interface RecipeManager extends ReactiveMongoRepository<Recipe, String>{
+	
+	Mono<Boolean> existsByTitleIgnoreCase(String title);
+	
+	Flux<Recipe> findByTitleContainingIgnoreCase(String title);
 
 }
